57abadd2a193c82b451f5266ed32bb0054f030f7
miopadrex <geunman.lee@solarconnect.kr>
2022-05-02 19:14:34 +0900

    test

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,11 @@
-// 프로젝트의 변동이 있을 때, 해당 파일에서 lint-staged를 수정하게 됩니다.
-module.exports = {
-  "*.+(ts|tsx)": [() => "yarn tsc -p tsconfig.json --noEmit"],
-  "packages/common-styles/**/*.+(ts|tsx)": [
-    () => "yarn tsc -p packages/common-styles/tsconfig.json --noEmit",
-  ],
-  "packages/test-code-study/**/*.+(ts|tsx)": [
-    () => "yarn tsc -p packages/prototype-a/tsconfig.json --noEmit",
-  ],
-  "**/*.+(ts|tsx|js|jsx)": ["eslint --fix --cache", "prettier --write"],
-};
+// // 프로젝트의 변동이 있을 때, 해당 파일에서 lint-staged를 수정하게 됩니다.
+// module.exports = {
+//   "*.+(ts|tsx)": [() => "yarn tsc -p tsconfig.json --noEmit"],
+//   "packages/common-styles/**/*.+(ts|tsx)": [
+//     () => "yarn tsc -p packages/common-styles/tsconfig.json --noEmit",
+//   ],
+//   "packages/test-code-study/**/*.+(ts|tsx)": [
+//     () => "yarn tsc -p packages/prototype-a/tsconfig.json --noEmit",
+//   ],
+//   "**/*.+(ts|tsx|js|jsx)": ["eslint --fix --cache", "prettier --write"],
+// };
